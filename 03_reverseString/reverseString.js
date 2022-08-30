b2e77c9ab415412896326052d0ca0d8b7ec2ef76
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-08-30 14:18:18 -0500

    Add solution to exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function (string) {
+    let splitArray = string.split('');
+    let finalArray = [];
+    for (let i = 0; i < splitArray.length; i++) {
+        finalArray.unshift(splitArray[i]);
+    }
+    return finalArray.join('');
 };
 
 // Do not edit below this line
