87ade3b43f7c4733baa31645fd5130ad3663b0d7
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-08-30 13:57:34 -0500

    Add solution to exercises

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function (string, num) {
+    let results = '';
+    for (let i = 0; i < num; i++) {
+        results += string;
+    }
+    if (num < 0) {
+        return 'ERROR'
+    }
+    return results;
 };
 
 // Do not edit below this line
