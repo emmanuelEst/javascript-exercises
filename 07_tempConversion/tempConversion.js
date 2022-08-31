09e7ca475e1e72e02f2ef949dec9d837ff5d6b5c
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-08-31 14:30:46 -0500

    Add solution for exercise

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function (celsius) {
+  return Math.round(((celsius - 32) * (5 / 9)) * 10) / 10;
 };
 
-const ctof = function() {
-
+const ctof = function (fahrenheit) {
+  return Math.round((fahrenheit * (9 / 5) + 32) * 10) / 10;
 };
 
 // Do not edit below this line
