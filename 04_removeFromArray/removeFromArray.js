e2a450b1ee9fe036199e934b246724a5c402efe9
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-08-30 21:55:30 -0500

    Add solution for exercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function (...params) {
+    let array = params[0];
+    let newArray = [];
 
+    array.forEach((item) => {
+        if (!params.includes(item)) {
+            newArray.push(item);
+        }
+    })
+    return newArray;
 };
 
 // Do not edit below this line
