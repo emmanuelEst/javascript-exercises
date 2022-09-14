b64d830bf00b68f4e16eb8e13b3684859cbedf85
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-09-14 13:59:57 -0500

    Add solution for exercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,25 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    let punctuation = [',', ' ', '.', '!']
+    let splitStr = string.toLowerCase().split('').filter(element => {
+        if (punctuation.includes(element)) {
+            return false;
+        } else {
+            return true;
+        }
+    });
 
+    let reversed = []
+    splitStr.forEach(element => {
+        reversed.unshift(element);
+    })
+    for (let i = 0; i < splitStr.length; i++) {
+        if (splitStr[i] != reversed[i]) {
+            return false;
+        }
+
+    }
+    return true;
 };
-
+palindromes('racecar!');
 // Do not edit below this line
 module.exports = palindromes;
