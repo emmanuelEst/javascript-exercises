a96df6123906c51255d1a11361f7d92013447040
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-09-14 18:30:04 -0500

    Add solution for exercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function (nth) {
+    if (nth < 0) {
+        return "OOPS";
+    }
+    let counter = 0;
+    let array = [1]
+    while (counter <= nth) {
+        if (array.length < 2) {
+            array.push(1);
+        } else {
+            array.push(array[counter - 1] + array[counter]);
+        }
+        counter++;
+    }
+    return array[Number(nth) - 1];
 };
 
 // Do not edit below this line
