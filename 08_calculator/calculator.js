dff060a3819549cc2ac8d36ca7fc4fd9b09f5368
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-09-13 22:42:22 -0500

    Add solutions for exercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function (a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function (a, b) {
+  return a - b;
 };
 
-const sum = function() {
-	
+const sum = function (array) {
+  let total = 0;
+  array.forEach(index => total += index);
+  return total;
 };
 
-const multiply = function() {
-
+const multiply = function (array) {
+  let multiplied = 1;
+  array.forEach(item => multiplied *= item);
+  return multiplied;
 };
 
-const power = function() {
-	
+const power = function (a, b) {
+  return a ** b;
 };
 
-const factorial = function() {
-	
+const factorial = function (num) {
+  if (num === 1 || num === 0) {
+    return 1;
+  }
+  let counter = num - 1;
+  let factorial = num;
+  while (counter >= 1) {
+    factorial *= counter;
+    counter--;
+  }
+  return factorial;
 };
 
 // Do not edit below this line
