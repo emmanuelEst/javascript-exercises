0955d81605bac9132f3ae7fe045975d3d45773ea
Emmanuel Estrella <estrellaemmanuel74@gmail.com>
2022-09-15 14:11:33 -0500

    Add solution for exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,21 @@
-const findTheOldest = function() {
-
+const findTheOldest = function (array) {
+    let oldest;
+    let age = 0;
+    let elementAge;
+    array.forEach(element => {
+        if ('yearOfDeath' in element) {
+            elementAge = element.yearOfDeath - element.yearOfBirth;
+        } else {
+            let currentYear = new Date().getFullYear();
+            elementAge = currentYear - element.yearOfBirth;
+        }
+        console.log(elementAge);
+        if (elementAge > age) {
+            age = elementAge
+            oldest = element;
+        }
+    });
+    return oldest;
 };
 
 // Do not edit below this line
